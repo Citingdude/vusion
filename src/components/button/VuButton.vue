a5e7c469ec341300119507ea33eedfb86233b39a
glenn <glenn.reumers@appwise.be>
2025-05-18 20:49:47 +0200

    feat: add tailwind-variants dependency and enhance VuButton with disabled and pending states

--- src/components/button/VuButton.vue
+++ src/components/button/VuButton.vue
@@ -1,20 +1,56 @@
 <script setup lang="ts">
+import { tv } from 'tailwind-variants'
+import { computed } from 'vue'
+import CircleLoaderIcon from '@/components/icon/CircleLoaderIcon.vue'
+
 const props = defineProps<{
   label: string
   type?: 'button' | 'reset' | 'submit'
+  isDisabled?: boolean
+  isPending?: boolean
 }>()
+
+const button = tv({
+  base: `text-white bg-black font-medium rounded-lg text-sm px-5 py-2.5
+  focus-visible:outline-2 focus-visible:outline-offset-4
+focus-visible:outline-black dark:focus-visible:outline-white
+hover:brightness-90 active:scale-95
+dark:bg-white dark:text-black w-fit
+transition duration-100`,
+  variants: {
+    disabled: {
+      true: 'opacity-50 pointer-events-none',
+      false: '',
+    },
+  },
+})
+
+const buttonClass = computed(() => {
+  return button({
+    disabled: props.isDisabled,
+  })
+})
 </script>
 
 <template>
   <button
     :type="props.type"
-    class="text-white bg-black font-medium rounded-lg text-sm px-5 py-2.5
-    focus-visible:outline-2 focus-visible:outline-offset-4
-    focus-visible:outline-black dark:focus-visible:outline-white
-    hover:brightness-90 active:scale-95
-    dark:bg-white dark:text-black
-    transition duration-100"
+    :class="buttonClass"
+    :disabled="props.isDisabled"
+    class="flex gap-2 items-center"
   >
-    {{ props.label }}
+    <CircleLoaderIcon
+      v-if="props.isPending"
+      aria-hidden="true"
+      class="size-4 animate-spin"
+    />
+
+    <slot v-if="!props.isPending" name="leadingIcon" />
+
+    <span class="max-w-xs overflow-ellipsis overflow-hidden text-nowrap">
+      {{ props.label }}
+    </span>
+
+    <slot name="trailingIcon" />
   </button>
 </template>
--- src/views/components/ButtonView.vue
+++ src/views/components/ButtonView.vue
@@ -1,37 +1,86 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import VuButton from '@/components/button/VuButton.vue'
+import ArrowUpRightIcon from '@/components/icon/ArrowUpRightIcon.vue'
 
 const label = ref<string>('Read more')
+const isDisabled = ref<boolean>(false)
+const isPending = ref<boolean>(false)
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col gap-8">
-    <div class="max-w-fit">
-      <label
-        for="label"
-        class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white"
-      >
-        Label
-      </label>
-      <input
-        id="label"
-        v-model="label"
-        type="text"
-        class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg
+    <div class="flex flex-col gap-4">
+      <div class="max-w-fit">
+        <label
+          for="label"
+          class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white"
+        >
+          Label
+        </label>
+        <input
+          id="label"
+          v-model="label"
+          type="text"
+          class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg
         block w-full p-2.5
         dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white
         focus-visible:outline-2
     focus-visible:outline-black dark:focus-visible:outline-white"
-        placeholder="Set label"
-        required
-      >
+          placeholder="Set label"
+        >
+      </div>
+
+      <div class="max-w-fit">
+        <label
+          for="label"
+          class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white"
+        >
+          Disabled
+        </label>
+        <input
+          id="label"
+          v-model="isDisabled"
+          type="checkbox"
+          class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg
+        block w-full p-2.5
+        dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white
+        focus-visible:outline-2
+    focus-visible:outline-black dark:focus-visible:outline-white"
+        >
+      </div>
+
+      <div class="max-w-fit">
+        <label
+          for="label"
+          class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white"
+        >
+          Pending
+        </label>
+        <input
+          id="label"
+          v-model="isPending"
+          type="checkbox"
+          class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg
+        block w-full p-2.5
+        dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white
+        focus-visible:outline-2
+    focus-visible:outline-black dark:focus-visible:outline-white"
+        >
+      </div>
     </div>
 
     <div>
       <VuButton
         :label="label"
-      />
+        :is-disabled="isDisabled"
+        :is-pending="isPending"
+        tag="button"
+      >
+        <template #trailingIcon>
+          <ArrowUpRightIcon class="size-4" />
+        </template>
+      </VuButton>
     </div>
   </div>
 </template>
