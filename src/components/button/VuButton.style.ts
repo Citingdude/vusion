db9a452d7f05c4ba9ad3a4800f1297fcbf934101
glenn <glenn.reumers@appwise.be>
2025-05-29 09:32:28 +0200

    feat(VuButton): enhance styling with improved hover effects and transition

--- src/components/button/VuButton.style.ts
+++ src/components/button/VuButton.style.ts
@@ -5,10 +5,11 @@ export const buttonStyle = tv({
     base: `
       flex gap-2 items-center text-white bg-black font-medium rounded-lg text-sm px-5 py-2.5
       focus-visible:outline-2 focus-visible:outline-offset-4
-    focus-visible:outline-black dark:focus-visible:outline-white
-      hover:brightness-90 active:scale-95
-    dark:bg-white dark:text-black w-fit
-      transition duration-100
+      focus-visible:outline-black dark:focus-visible:outline-white
+      hover:bg-neutral-800
+      hover:shadow-black active:scale-95 
+      dark:bg-white dark:text-black w-fit dark:hover:bg-neutral-200
+      transition duration-100 ease-in-out
     `,
     label: 'max-w-xs overflow-ellipsis overflow-hidden text-nowrap',
   },
